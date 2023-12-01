9c252d8686c554e757bf1ad51f16f8c02142677f
Elmin Didic <35606145+ElminD@users.noreply.github.com>
2023-12-01 14:11:11 -0600

    Remove unnecessary code and optimize string manipulation

--- Day01/src/Part2.java
+++ Day01/src/Part2.java
@@ -9,7 +9,6 @@ public class Part2 {
            File myObj = new File("Day01/src/input.txt");
            Scanner myReader = new Scanner(myObj);
 
-           int lines = 0;
            String[] numbers = {
                 "zero0", "one1", "two2", "three3", "four4", "five5", "six6", "seven7", "eight8", "nine9"
             };
@@ -18,7 +17,6 @@ public class Part2 {
               String data = myReader.nextLine();
               String first = "";
               String last = "";
-              lines++;
 
               for(int i = numbers.length - 1; i > -1; i--){
                 data = data.replace(numbers[i].substring(0, numbers[i].length()-1),numbers[i].substring(0, numbers[i].length()-1) + numbers[i].charAt(numbers[i].length()- 1) + numbers[i].substring(0, numbers[i].length()-1));
