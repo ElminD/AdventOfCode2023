398890b02c314128597bf3c11aa8c4df4299195e
Elmin Didic <35606145+ElminD@users.noreply.github.com>
2023-12-01 14:10:18 -0600

    Part1 and Part2 done

---
+++ Day01/src/Part1.java
@@ -0,0 +1,39 @@
+import java.io.File; 
+import java.io.FileNotFoundException;
+import java.util.Scanner; 
+
+public class Part1 {
+    public static void main(String[] args) throws Exception {
+        int total = 0;
+        try {
+            File myObj = new File("Day01/src/input.txt");
+            Scanner myReader = new Scanner(myObj);
+    
+            while (myReader.hasNextLine()) {
+              String data = myReader.nextLine();
+              String first = "", last = "";
+
+              for(char c : data.toCharArray()){
+                if(Character.isDigit(c)){
+                  first = c + "";
+                  break;
+                }
+              }      
+
+              for(char c : data.toCharArray()){
+                if(Character.isDigit(c)){
+                  last = c + "";
+                }
+              }
+
+              int digit = Integer.parseInt(first + last);
+              total += digit;
+            }
+            System.out.println("Total: " + total);
+            myReader.close();
+          } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+          }
+    }
+}
---
+++ Day01/src/Part2.java
@@ -0,0 +1,50 @@
+import java.io.File; 
+import java.io.FileNotFoundException;
+import java.util.Scanner; 
+
+public class Part2 {
+    public static void main(String[] args) throws Exception {
+        int total = 0;
+        try {
+           File myObj = new File("Day01/src/input.txt");
+           Scanner myReader = new Scanner(myObj);
+
+           int lines = 0;
+           String[] numbers = {
+                "zero0", "one1", "two2", "three3", "four4", "five5", "six6", "seven7", "eight8", "nine9"
+            };
+            
+            while (myReader.hasNextLine()) {
+              String data = myReader.nextLine();
+              String first = "";
+              String last = "";
+              lines++;
+
+              for(int i = numbers.length - 1; i > -1; i--){
+                data = data.replace(numbers[i].substring(0, numbers[i].length()-1),numbers[i].substring(0, numbers[i].length()-1) + numbers[i].charAt(numbers[i].length()- 1) + numbers[i].substring(0, numbers[i].length()-1));
+              }
+
+              for(char c : data.toCharArray()){
+                if(Character.isDigit(c)){
+                  first = c + "";
+                  break;
+                }
+              }
+              
+              for(char c : data.toCharArray()){
+                if(Character.isDigit(c)){
+                  last = c + "";
+                }
+              }
+
+              int digit = Integer.parseInt(first + last);
+              total += digit;
+            }
+            System.out.println("Total: " + total);
+            myReader.close();
+          } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+          }
+    }
+}
