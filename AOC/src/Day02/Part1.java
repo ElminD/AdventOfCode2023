427a1586a98e7efac6b0b55267e14ec194443991
Elmin Didic <35606145+ElminD@users.noreply.github.com>
2023-12-02 19:22:43 -0600

    Fix gameMap calculation in Part1.java

--- AOC/src/Day02/Part1.java
+++ AOC/src/Day02/Part1.java
@@ -15,6 +15,42 @@ public class Part1 {
     
             while (myReader.hasNextLine()) {
 
+                int id = 0;
+                int red = 0;
+                int redMax = 0;
+                int green = 0;
+                int greenMax = 0;
+                int blue = 0;
+                int blueMax = 0;
+
+                String game = myReader.nextLine();
+                game += ';';
+                for(int i = 0; i < game.length(); i++){
+                    if(game.charAt(i) == ':'){
+                        id = Character.valueOf(game.charAt(i - 1));
+                    }
+
+                    if(game.charAt(i) == ';'){
+                        redMax = Math.max(red, redMax);
+                        greenMax = Math.max(green, greenMax);
+                        blueMax = Math.max(blue, blueMax);
+                    }
+                    else{
+                      if(game.charAt(i) == 'r'){
+                          red = Character.valueOf(game.charAt(i - 2));
+                      }
+                      if(game.charAt(i) == 'g'){
+                          green = Character.valueOf(game.charAt(i - 2));
+                      }
+                      if(game.charAt(i) == 'b'){
+                          blue = Character.valueOf(game.charAt(i - 2));
+                      }
+                    }
+
+                } 
+
+                gameMap.put(id, redMax + "," + greenMax + "," + blueMax);
+                System.out.println(id + " " + redMax + "," + greenMax + "," + blueMax);
                 
              
             }
