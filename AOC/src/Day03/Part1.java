8ca26c5495b683811462277adbf9f1929b6fb1b7
Elmin Didic <35606145+ElminD@users.noreply.github.com>
2023-12-05 18:13:56 -0600

    Fix counting logic in main method

--- AOC/src/Day03/Part1.java
+++ AOC/src/Day03/Part1.java
@@ -5,8 +5,9 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Part1 {
-    public static void main(String[] args) throws Exception {
-         char[][] engine = new char[140][140];
+
+    public void main(String[] args) throws Exception {
+        char[][] engine = new char[140][140];
         try {
             File myObj = new File("AOC/src/Day03/input.txt");
             Scanner myReader = new Scanner(myObj);
@@ -14,13 +15,14 @@ public class Part1 {
             for(int row = 0; myReader.hasNextLine() && row < 140; row++) {
                 engine[row] = myReader.nextLine().toCharArray();
             }
+
+            int count = 0;
             
             for(int row = 0; row < 140; row++) {
                 for(int col = 0; col < 140; col++) {
-
-                    if(engine[row][col] == 'L') {
-                        engine[row][col] = '#';
-                    }
+                    if((engine[row][col] != '.') && (!Character.isDigit(engine[row][col]))) {                 
+                        count += Integer.parseInt(addNumbers(engine, row, col));
+                     }
 
                     System.out.print(engine[row][col]);
                 }
@@ -33,4 +35,25 @@ public class Part1 {
             e.printStackTrace();
           }
   }
+
+    String addNumbers(int[][] engine, int row, int col){
+        int ogRow = row;
+        int ogCol = col;
+        String total = "";
+
+        if(Character.isDigit(engine[row - 1][col - 1])) {
+            while(Character.isDigit(engine[row][col - 1])) {
+                col--;
+            }
+
+            while(Character.isDigit(engine[row][col])) {
+                total += engine[row][col];
+                col++;
+                engine[row][col] = '.';
+            }
+
+        }
+
+        return total;
+    }
 }
